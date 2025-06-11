05dfa323b8aa320a6dbd88ac1bfb3040ecaf40da
Pontinn <leo.pontin2@gmail.com>
2025-06-11 13:11:40 -0300

    first commit

---
+++ src/main/java/dev/pontin/DiabloBossLootList/DiabloBossLootListApplication.java
@@ -0,0 +1,13 @@
+package dev.pontin.DiabloBossLootList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabloBossLootListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabloBossLootListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pontin/DiabloBossLootList/DiabloBossLootListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pontin.DiabloBossLootList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabloBossLootListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
