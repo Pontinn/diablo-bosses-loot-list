5af651603bafef0da33ecc2ecc4dc35b214687ba
Pontinn <leo.pontin2@gmail.com>
2025-06-12 15:32:09 -0300

    feat: add files: ClassModel and ClassNames

--- src/main/java/dev/pontin/DiabloBossLootList/Classes/ClassModel.java
+++ src/main/java/dev/pontin/DiabloBossLootList/Classes/ClassModel.java
@@ -1,4 +1,32 @@
 package dev.pontin.DiabloBossLootList.Classes;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+@Table(name = "tb_class")
 public class ClassModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "class_name")
+    private ClassNames classNames;
+    
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Transient
+    public String getClassNames() {
+        return classNames != null ? classNames.getName() : null;
+    }
+
+
 }
--- src/main/java/dev/pontin/DiabloBossLootList/Classes/ClassNames.java
+++ src/main/java/dev/pontin/DiabloBossLootList/Classes/ClassNames.java
@@ -1,4 +1,27 @@
 package dev.pontin.DiabloBossLootList.Classes;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
 public enum ClassNames {
+    SPIRITBORN("Spiritborn"),
+    BARBARIAN("Barbarian"),
+    SORCERER("Sorcerer"),
+    NECROMANCER("Necromancer"),
+    DRUID("Druid"),
+    ROGUE("Rogue");
+
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
