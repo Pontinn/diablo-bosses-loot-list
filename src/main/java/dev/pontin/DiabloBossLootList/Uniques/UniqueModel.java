3c343d61bc5614d1a942046e4b180ef2ede172b3
Pontinn <leo.pontin2@gmail.com>
2025-06-13 10:00:56 -0300

    feat: add unique columns

--- src/main/java/dev/pontin/DiabloBossLootList/Uniques/UniqueModel.java
+++ src/main/java/dev/pontin/DiabloBossLootList/Uniques/UniqueModel.java
@@ -1,7 +1,6 @@
 package dev.pontin.DiabloBossLootList.Uniques;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,10 +13,45 @@ import lombok.NoArgsConstructor;
 @Table(name = "tb_unique")
 public class UniqueModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "unique_name")
     private String name;
 
+    @Column(name = "img_url")
     private String imgUrl;
-    
+
+    @Column(name = "type")
+    private ItemType itemType;
+
+    @Column(name = "rarity")
+    private ItemRarity itemRarity;
+
+    @Column(name = "propriety_1")
+    private String itemPropriety1;
+
+    @Column(name = "propriety_2")
+    private String itemPropriety2;
+
+    @Column(name = "affix_1")
+    private String itemAffix1;
+
+    @Column(name = "affix_2")
+    private String itemAffix2;
+
+    @Column(name = "affix_3")
+    private String itemAffix3;
+
+    @Column(name = "affix_4")
+    private String itemAffix4;
+
+    @Column(name = "aspect")
+    private String itemAspect;
+
+    @Transient
+    public String getItemType() {
+        return itemType != null ? itemType.getName() : null;
+    }
 }
