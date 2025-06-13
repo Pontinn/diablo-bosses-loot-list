953559e04f0e7468a00fbff9a2b5a62536e3ccf6
Pontinn <leo.pontin2@gmail.com>
2025-06-13 16:12:21 -0300

    feat: add findByName method in BossRepository

--- src/main/java/dev/pontin/DiabloBossLootList/Bosses/BossRepository.java
+++ src/main/java/dev/pontin/DiabloBossLootList/Bosses/BossRepository.java
@@ -2,5 +2,9 @@ package dev.pontin.DiabloBossLootList.Bosses;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BossRepository extends JpaRepository<BossModel, Long> {
+
+    Optional<BossModel> findByName(String nome);
 }
