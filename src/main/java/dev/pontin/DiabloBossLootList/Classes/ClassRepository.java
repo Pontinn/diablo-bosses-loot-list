c0c18f9dbcea298a9908443df1d18d3c4a3bc0d2
Pontinn <leo.pontin2@gmail.com>
2025-06-13 16:13:02 -0300

    feat: add finByClassNames method in ClassRepository

--- src/main/java/dev/pontin/DiabloBossLootList/Classes/ClassRepository.java
+++ src/main/java/dev/pontin/DiabloBossLootList/Classes/ClassRepository.java
@@ -3,4 +3,6 @@ package dev.pontin.DiabloBossLootList.Classes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClassRepository extends JpaRepository<ClassModel, Long> {
+
+    ClassModel findByClassNames(ClassNames className);
 }
