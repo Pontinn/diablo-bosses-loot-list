8450a74d6396a5e04e5fc1eb1e4a2051ddc2762a
Pontinn <leo.pontin2@gmail.com>
2025-06-13 16:14:38 -0300

    feat: create UniqueSeedDTO to populate my database through a JSON file

--- src/main/java/dev/pontin/DiabloBossLootList/DTO/UniqueSeedDTO.java
+++ src/main/java/dev/pontin/DiabloBossLootList/DTO/UniqueSeedDTO.java
@@ -1,4 +1,23 @@
 package dev.pontin.DiabloBossLootList.DTO;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class UniqueSeedDTO {
+    private String name;
+    private String imgUrl;
+    private String itemType;
+    private String itemRarity;
+    private String itemPropriety1;
+    private String itemPropriety2;
+    private String itemAffix1;
+    private String itemAffix2;
+    private String itemAffix3;
+    private String itemAffix4;
+    private String itemAspect;
+
+    private List<String> bosses;
+    private List<String> classes;
 }
